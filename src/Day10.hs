5ab94c7afca2c689841abdb5bc69005a705a3309
J. Nelson <drumhacker72@gmail.com>
2020-12-10 16:58:28 -0600

    more space-efficient memoization for day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,31 +1,27 @@
 module Day10(Day10) where
 
-import Data.List (sort)
+import Data.List (zipWith4)
+import qualified Data.Set as S
 import Day
 
 type Adapter = Int
+readAdapters = S.fromList . map read . lines
 
-downFrom :: [Adapter] -> Adapter
-downFrom adapters = df 0
-  where
-    df = (map df' [0..] !!)
-    df' :: Int -> Int
-    df' i
-        | i == length adapters - 1 = 1
-        | otherwise =
-            let (a:as) = drop i adapters
-                nb = if length as > 0 && as !! 0 `elem` [a-1, a-2, a-3] then df (i+1) else 0
-                nc = if length as > 1 && as !! 1 `elem` [a-2, a-3] then df (i+2) else 0
-                nd = if length as > 2 && as !! 2 `elem` [a-3] then df (i+3) else 0
-             in nb + nc + nd
-
-diff (a1:a2:as) = (a1-a2) : diff (a2:as)
+diff (a1:a2:as) = (a2-a1) : diff (a2:as)
 diff _ = []
 
-newtype Day10 = D10 { runD10 :: [Adapter] }
+paths :: S.Set Adapter -> Int
+paths adapters = paths' !! (S.findMax adapters + 2)
+  where
+    paths' = 0 : 0 : 1 : zipWith4 next [1..] paths' (drop 1 paths') (drop 2 paths')
+    next i p3 p2 p1
+        | i `S.member` adapters = p3 + p2 + p1
+        | otherwise = 0
+
+newtype Day10 = D10 { runD10 :: S.Set Adapter }
 instance Day Day10 where
-    readDay _ = D10 . reverse . (0:) . sort . map (read :: String -> Int) . lines
-    part1 (D10 adapters) =
-        let adapters' = maximum adapters + 3 : adapters
-         in show $ length (filter (== 1) $ diff adapters') * length (filter (== 3) $ diff adapters')
-    part2 = show . downFrom . runD10
+    readDay _ = D10 . readAdapters
+    part1 (D10 as) =
+        let as' = 0 : S.toAscList as ++ [S.findMax as + 3]
+         in show $ length (filter (== 1) $ diff as') * length (filter (== 3) $ diff as')
+    part2 = show . paths . runD10
