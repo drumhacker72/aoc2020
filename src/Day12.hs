0d9b6b7ccf435f23d51092cbd243e063e19a55c2
J. Nelson <drumhacker72@gmail.com>
2020-12-12 01:13:19 -0600

    clean up day 12 logic with state

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,50 +1,55 @@
 module Day12(Day12) where
 
+import Control.Monad (foldM)
+import Control.Monad.Trans.State (evalState, get, modify, put)
 import Day
 
-data Nav = Nav Action Int
+type Nav = (Action, Int)
 data Action = N | S | E | W | L | R | F
     deriving (Read, Show)
 
-readNav (a:n) = Nav (read [a]) (read n)
+readNav (a:n) = (read [a], read n)
 
-data Loc1 = Loc1 Int Int Int
+move1 p (F, n) = do
+    r <- get
+    let a' = case r `mod` 360 of
+            0 -> E
+            90 -> N
+            180 -> W
+            270 -> S
+    move1 p (a', n)
+move1 p@(x, y) (a, n) = case a of
+    N -> return (x, y+n)
+    S -> return (x, y-n)
+    E -> return (x+n, y)
+    W -> return (x-n, y)
+    L -> modify (+ n) >> return p
+    R -> modify (subtract n) >> return p
 
-move1 (Loc1 x y r) (Nav a n) = case a of
-    N -> Loc1 x (y+n) r
-    S -> Loc1 x (y-n) r
-    E -> Loc1 (x+n) y r
-    W -> Loc1 (x-n) y r
-    L -> Loc1 x y (r-n)
-    R -> Loc1 x y (r+n)
-    F -> case r `mod` 360 of
-        0 -> Loc1 (x+n) y r
-        90 -> Loc1 x (y-n) r
-        180 -> Loc1 (x-n) y r
-        270 -> Loc1 x (y+n) r
+move2 (sx, sy) (F, n) = do
+    (wx, wy) <- get
+    return (sx+wx*n, sy+wy*n)
+move2 sp (a, n) = do
+    (wx, wy) <- get
+    case a of
+        N -> put (wx, wy+n)
+        S -> put (wx, wy-n)
+        E -> put (wx+n, wy)
+        W -> put (wx-n, wy)
+        L -> case n of
+            90 -> put (-wy, wx)
+            180 -> put (-wx, -wy)
+            270 -> put (wy, -wx)
+        R -> case n of
+            90 -> put (wy, -wx)
+            180 -> put (-wx, -wy)
+            270 -> put (-wy, wx)
+    return sp
 
-data Loc2 = Loc2 Int Int Int Int
-
-move2 (Loc2 sx sy wx wy) (Nav a n) = case a of
-    N -> Loc2 sx sy wx (wy+n)
-    S -> Loc2 sx sy wx (wy-n)
-    E -> Loc2 sx sy (wx+n) wy
-    W -> Loc2 sx sy (wx-n) wy
-    L -> case n of
-        90 -> Loc2 sx sy (-wy) wx
-        180 -> Loc2 sx sy (-wx) (-wy)
-        270 -> Loc2 sx sy wy (-wx)
-    R -> case n of
-        90 -> Loc2 sx sy wy (-wx)
-        180 -> Loc2 sx sy (-wx) (-wy)
-        270 -> Loc2 sx sy (-wy) wx
-    F -> Loc2 (sx+wx*n) (sy+wy*n) wx wy
-
-manhattan1 (Loc1 x y _) = abs x + abs y
-manhattan2 (Loc2 sx sy _ _) = abs sx + abs sy
+manhattan (x, y) = abs x + abs y
 
 newtype Day12 = D12 { runD12 :: [Nav] }
 instance Day Day12 where
     readDay _ = D12 . map readNav . lines
-    part1 = show . manhattan1 . foldl move1 (Loc1 0 0 0) . runD12
-    part2 = show . manhattan2 . foldl move2 (Loc2 0 0 10 1) . runD12
+    part1 = show . manhattan . (`evalState` 0) . foldM move1 (0, 0) . runD12
+    part2 = show . manhattan . (`evalState` (10, 1)) . foldM move2 (0, 0) . runD12
