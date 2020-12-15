31199a0403435d3bd991dd95348f6e564d42f209
J. Nelson <drumhacker72@gmail.com>
2020-12-15 14:51:15 -0600

    day 15 cleanup (rewrite as fold)

--- src/Day15.hs
+++ src/Day15.hs
@@ -2,29 +2,22 @@
 
 module Day15(Day15) where
 
+import Control.Monad (foldM, forM_)
 import Control.Monad.ST (runST)
 import Data.List.Split (splitOn)
 import Data.Vector.Generic.Mutable (exchange, write)
 import qualified Data.Vector.Unboxed.Mutable as VUM
 import Day
 
-next is i n = do
+next is n i = do
     prev <- exchange is n i
     return $ if prev == 0 then 0 else i - prev
 
 run end ns = do
     is <- VUM.replicate end 0
-    (i, n) <- fill is 1 ns
-    loop is i n
-  where
-    fill _  i [n]    = return (i, n)
-    fill is i (n:ns) = write is n i >> fill is (i+1) ns
-
-    loop is i n
-        | i == end  = return n
-        | otherwise = do
-            n' <- next is i n
-            loop is (i+1) n'
+    let (n, i):inits = reverse $ zip ns [1..]
+    forM_ inits $ uncurry $ write is
+    foldM (next is) n [i..end-1]
 
 newtype Day15 = D15 [Int]
 instance Day Day15 where
