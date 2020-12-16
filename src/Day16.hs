4aad0655d900a874e35373f02e2bae1179058a7c
J. Nelson <drumhacker72@gmail.com>
2020-12-16 01:59:44 -0600

    day 16 cleanup

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,12 +1,9 @@
-module Day16 where --(Day16) where
+module Day16(Day16) where
 
-import Control.Monad (guard)
-import Data.Char (isDigit, isLower)
+import Data.Char (isDigit)
 import Data.Foldable (toList)
-import Data.IntSet (IntSet)
-import Data.List (isPrefixOf, permutations)
-import Data.Sequence (Seq((:|>), (:<|), Empty), (><))
-import qualified Data.IntSet as IS
+import Data.List (isPrefixOf, transpose)
+import Data.Sequence (Seq((:<|), Empty), (><))
 import qualified Data.Sequence as S
 import qualified Text.ParserCombinators.ReadP as P
 import Day
@@ -16,7 +13,7 @@ type Ticket = [Int]
 
 number = read <$> P.munch1 isDigit
 rule = do
-    name <- P.munch1 $ \c -> isLower c || c == ' '
+    name <- P.munch1 (/= ':')
     P.string ": "
     n1 <- number
     P.char '-'
@@ -26,9 +23,7 @@ rule = do
     P.char '-'
     n4 <- number
     return $ Rule name $ \i -> (n1 <= i && i <= n2) || (n3 <= i && i <= n4)
-
 ticket = P.sepBy1 number (P.char ',')
-
 input = do
     rules <- P.sepBy1 rule (P.char '\n')
     P.string "\n\nyour ticket:\n"
@@ -39,36 +34,32 @@ input = do
     P.eof
     return (rules, your, nearby)
 
-canBeValid preds v = any ($ v) preds
+canBeValid rules v = any (`predicate` v) rules
 
-ticketErrorRate preds ticket =
-    sum $ map (\v -> if canBeValid preds v then 0 else v) ticket
+errorRate rules ticket =
+    sum $ map (\v -> if canBeValid rules v then 0 else v) ticket
 
-validTicket preds = all (canBeValid preds)
+validOrders [] Empty = [S.empty]
+validOrders (ticketSlice:remainingSlices) rules =
+    [ rule :<| validRemainingOrder
+    | i <- [0 .. length rules - 1]
+    , let (left, rule :<| right) = S.splitAt i rules
+          remainingRules = left >< right
+    , all (predicate rule) ticketSlice
+    , validRemainingOrder <- validOrders remainingSlices remainingRules
+    ]
 
-validOrders :: IntSet -> Int -> [Seq Int] -> Seq Rule -> [Seq Rule]
-validOrders used t tickets rules
-    | IS.size used == length rules = [S.empty]
-    | otherwise = do
-        r <- [0 .. length rules - 1]
-        guard $ r `IS.notMember` used
-        let rule = rules `S.index` r
-        guard $ all (predicate rule . (`S.index` t)) tickets
-        rest' <- validOrders (IS.insert r used) (t+1) tickets rules
-        return $ rule :<| rest'
+findOrder tickets rules =
+    toList $ head $ validOrders (transpose tickets) (S.fromList rules)
 
 data Day16 = D16 [Rule] Ticket [Ticket]
 instance Day Day16 where
     readDay _ s =
         let [((rules, your, nearby), "")] = P.readP_to_S input s
          in D16 rules your nearby
-    part1 (D16 rules _ nearby) =
-        let preds = map predicate rules
-         in show $ sum $ map (ticketErrorRate preds) nearby
+    part1 (D16 rules _ nearby) = show $ sum $ map (errorRate rules) nearby
     part2 (D16 rules your nearby) =
-        let preds = map predicate rules
-            nearby' = filter (validTicket preds) nearby
-            rules' = toList $ head $ validOrders IS.empty 0 (map S.fromList (your:nearby')) $ S.fromList rules
-            your' = zip (map field rules') your
-            departures = filter (("departure" `isPrefixOf`) . fst) your'
-         in show $ product $ map snd departures
+        let validNearby = filter (all (canBeValid rules)) nearby
+            fields = map field $ findOrder (your:validNearby) rules
+            departureProps = filter (("departure" `isPrefixOf`) . fst) $ zip fields your
+         in show $ product $ map snd departureProps
